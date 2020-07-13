73671a06a4973a5a1bc3c301d5d660d1fa141cd5
Martin Barnáš <martin.barnas@avast.com>
2020-07-13 10:58:43 +0200

    Initial Commit

---
+++ Mzda 2020/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mzda 2020
+//
+//  Created by Martin Barnas on 13/07/2020.
+//  Copyright © 2020 Martin Barnas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mzda 2020Tests/Mzda_2020Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Mzda_2020Tests.swift
+//  Mzda 2020Tests
+//
+//  Created by Martin Barnas on 13/07/2020.
+//  Copyright © 2020 Martin Barnas. All rights reserved.
+//
+
+import XCTest
+@testable import Mzda_2020
+
+class Mzda_2020Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
